838f3918bc79fd17051e2d69c1ed2fd0ce167e32
Tzuen Voo <vince_voo@yahoo.com>
2023-06-15 13:05:56 +0800

    model for user-username, review-modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   has_many :orders
+  has_many :favorites
+  has_many :products
+  has_many :reviews, through: :orders
 end
