64a996e2900e01aa9b552385509cfbaf5a3b9449
ChangJie Tan <tan7cj@gmail.com>
2023-06-15 09:34:38 +0800

    category product product_act

---
+++ db/migrate/20230615012418_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name, null: false
+      t.float :price, null: false
+      t.integer :quantity, null: false
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
