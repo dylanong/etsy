de4c615405e8f34aecd299149522f59d3575a11c
ChangJie Tan <tan7cj@gmail.com>
2023-06-15 13:06:12 +0800

    update product,cat model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
+
+  has_many :favourites
+  has_many :line_items
+  has_many :orders, through: :line_items
+  has_many :reviews, through: :orders, dependent: :destroy
+  has_many :product_categories
+  has_many :products, through: :product_categories
 end
--- db/migrate/20230615012418_create_products.rb
+++ db/migrate/20230615012418_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
     create_table :products do |t|
       t.string :name, null: false
       t.float :price, null: false
-      t.integer :quantity, null: false
+      t.integer :quantity, null: false, default: 0
       t.text :description
       t.references :user, null: false, foreign_key: true
 
