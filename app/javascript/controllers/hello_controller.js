abb97be3ca2f5659517f9f715913c84be1f6916f
Tzuen Voo <vince_voo@yahoo.com>
2023-06-19 11:29:34 +0800

    update on nav

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,23 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static target = ["menu"];
+
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "Hello World!";
+    this.menuElement = this.findMenuElement();
+    this.hideMenu();
+  }
+  findMenuElement() {
+    // Implement your logic to find the menu element, e.g., by ID or class name
+    return document.getElementById("menu");
+  }
+
+  showMenu() {
+    this.menuElement.classList.add("show");
+  }
+
+  hideMenu() {
+    this.menuElement.classList.remove("show");
   }
 }
