f7db8e0fbec1b173b8da613f587c4d7b875aa8f7
Dylan Ong <dylan.ong@icloud.com>
2023-06-19 16:31:00 +0800

    added quantity and color dropdown menu add to basket is not fixed yet

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ApplicationRecord
-QTYALLOWED = [1, 2, 3, 4, 5, 6, 7, 8]
-
   belongs_to :user
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
